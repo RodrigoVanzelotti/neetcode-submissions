dd8b1619e6bf2114471c8b45445d6648565ec004
Rodrigo Vanzelotti <rodrigovanzelotti@gmail.com>
2026-07-29 19:15:47 -0300

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        highest_profit = 0
+
+        prices_len = len(prices)
+
+        if prices_len == 1: return profit
+
+        for i in range(prices_len):
+            if i == prices_len - 1: break
+
+            for j in range(i, prices_len):
+                print(f'entered the loop {i} {j}')
+                if i == j: continue
+                if prices[i] > prices[j]: break
+
+                op_value = prices[j] - prices[i] 
+                print(f'{op_value} = {prices[i]} - {prices[j]}')
+                print(f'current profit {profit}')
+                if op_value > profit:
+                    print(f'update profit on iteration {i} {j}')
+                    profit = op_value
+
+
+        return profit
+
+            
+            # [5,2,3,4,3,8,1,9]
+            
